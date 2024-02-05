138a9ccec4cecbc1cc42ce9f5a7ea666d5e79548
Rached Mejri <r.mejri74100@gmail.com>
2024-02-05 14:40:39 +0100

    suppression gitignore

--- db/run.sh
+++ db/run.sh
@@ -3,11 +3,11 @@ docker network create app-network > /dev/null;
 docker stop postgres > /dev/null;
 docker rm postgres > /dev/null;
 docker build -t postgres . --no-cache > /dev/null && 
-docker run -d -p 5432:5432 --name postgres postgres > /dev/null &&
+docker run -d -p 5432:5432 --network app-network --name postgres postgres > /dev/null &&
 echo "Postgres is running ..."
 
 # Configuration d'adminer
 docker stop adminer > /dev/null;
 docker rm adminer > /dev/null; 
-docker run -d --link postgres:db -p 9090:8080 --name adminer adminer 1> /dev/null
+docker run -d --network app-network --link postgres:db -p 9090:8080 --name adminer adminer 1> /dev/null
 echo "Adminer is running ..."
--- webserver/run.sh
+++ webserver/run.sh
@@ -3,5 +3,5 @@ docker network create app-network > /dev/null;
 docker stop webserver > /dev/null;
 docker rm webserver > /dev/null;
 docker build -t webserver . --no-cache > /dev/null && 
-docker run -d -p 7070:80 --name webserver webserver > /dev/null &&
+docker run -d -p 7070:80 --network app-network --name webserver webserver > /dev/null &&
 echo "Webserver is running ..."
