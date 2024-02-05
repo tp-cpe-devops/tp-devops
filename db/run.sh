cfff64122c5c976637654ecfe50af2219fb1a139
Rached Mejri <r.mejri74100@gmail.com>
2024-02-05 11:02:38 +0100

    configuration d'adminer

--- db/run.sh
+++ db/run.sh
@@ -1,5 +1,13 @@
-docker network create app-network;
-docker stop postgres &&
-docker rm postgres && 
-docker build -t postgres . --no-cache && 
-docker run -it -p 5432:5432 --name postgres postgres
+# Configuration de postgres
+docker network create app-network > /dev/null;
+docker stop postgres > /dev/null &&
+docker rm postgres > /dev/null && 
+docker build -t postgres . --no-cache > /dev/null && 
+docker run -d -p 5432:5432 --name postgres postgres > /dev/null &&
+echo "Postgres is running ..."
+
+# Configuration d'adminer
+docker stop adminer > /dev/null;
+docker rm adminer > /dev/null; 
+docker run -d --link postgres:db -p 8080:8080 --name adminer adminer 1> /dev/null
+echo "Adminer is running ..."
