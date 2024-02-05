9fcc7c8d789dc02f7f8962c20299fb73f74c2e3b
Rached Mejri <r.mejri74100@gmail.com>
2024-02-05 11:41:52 +0100

    mise en place de webserver

---
+++ backend/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,10 @@
+
+:
+
+Dockerfile,6/6/6651ddff6eb82c840ced7c1dddee15c6e1913dd4
+A
+simpleapi/pom.xml,1/b/1b494ba4394a828c54a0e66245413313bc883c1f
+6
+run.sh,3/2/324fcb49f1cf313ebdd7cbda6a71b73727ddd94d
+~
+Nsimpleapi/src/main/java/fr/takima/training/simpleapi/SimpleapiApplication.java,a/8/a8998406e32aa27447dc9b0bdae18d3f19d1c7c1
--- backend/run.sh
+++ backend/run.sh
@@ -1,8 +1,7 @@
-# Configuration de postgres
+# Configuration du backend
 docker network create app-network > /dev/null;
 docker stop backend > /dev/null;
 docker rm backend > /dev/null;
 docker build -t backend . --no-cache > /dev/null && 
-docker run -d -p 5432:5432 --name backend backend > /dev/null &&
+docker run -d -p 8080:8080 --name backend backend > /dev/null &&
 echo "Backend is running ..."
-    
