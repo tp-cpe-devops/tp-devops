ca17ba63dc9236e6294da28f97a7ed07d3fe0a5d
Rached Mejri <r.mejri74100@gmail.com>
2024-02-05 10:52:11 +0100

    modif du .sh

--- db/run.sh
+++ db/run.sh
@@ -1,4 +1,5 @@
 docker network create app-network;
-docker rmi postgres --force &&
+docker stop postgres &&
+docker rm postgres && 
 docker build -t postgres . --no-cache && 
 docker run -it -p 5432:5432 --name postgres postgres
