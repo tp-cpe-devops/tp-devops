3efca8ec8ed5aaa269cd56a77d5e34ae556d016b
baptiste-gris <gris.baptiste@gmail.com>
2024-02-08 14:32:09 +0100

    remove: backend-2

--- backend-2/run_unix.sh
+++
@@ -1,7 +0,0 @@
-# Configuration du backend
-docker network create app-network > /dev/null;
-docker stop backend-2 > /dev/null;
-docker rm backend-2 > /dev/null;
-docker build -t backend-2 . --no-cache > /dev/null && 
-docker run -d -p 8090:8090 --network app-network --name backend-2 backend-2 > /dev/null &&
-echo "Backend 2 is running ..."
--- backend-2/tr.sh
+++
@@ -1,3 +0,0 @@
-tr -d '\r' < run.sh > run_unix.sh
-chmod +x run_unix.sh
-./run_unix.sh
